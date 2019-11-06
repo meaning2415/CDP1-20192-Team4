e39a51ab79cf9081576df96843a1ea5791d98425
Cappuccinomild <33108600+Cappuccinomild@users.noreply.github.com>
2019-11-06 13:47:52 +0900

    Add files via upload

---
+++ pad.py
@@ -0,0 +1,78 @@
+from tkinter import *
+from tkinter import ttk
+from socket import *
+
+global clientSock
+
+def button_pressed(value):
+    number_entry.insert("end",value)
+    clientSock.send(str(value).encode())
+    print(value,"pressed")
+     
+def star_button_pressed(value):
+    print(value,"pressed")
+    clientSock.send(str(value).encode())
+    result = clientSock.recv(1).decode()
+    print(result)
+    
+def num_button_pressed():
+    print("# pressed")
+def recvall(sock, count):
+    buf = b''
+    while count:
+        newbuf = sock.recv(count)
+        if not newbuf: return None
+        buf += newbuf
+        count -= len(newbuf)
+    return buf
+
+
+address = 'localhost'
+port = 8080
+
+clientSock = socket(AF_INET, SOCK_STREAM)
+clientSock.connect((address, port))
+
+root = Tk()
+w, h = root.winfo_screenwidth(), root.winfo_screenheight()
+root.overrideredirect(1)
+root.geometry("%dx%d+0+0" % (w, h))
+ 
+entry_value = StringVar(root, value='')
+ 
+number_entry = ttk.Entry(root, textvariable = entry_value, width=20)
+number_entry.grid(row=0, columnspan=3) 
+ 
+ 
+button7 = ttk.Button(root, text="7", command = lambda:button_pressed('7'))
+button7.grid(row=1, column=0,sticky='NSEW')
+button8 = ttk.Button(root, text="8", command = lambda:button_pressed('8'))
+button8.grid(row=1, column=1,sticky='NSEW')
+button9 = ttk.Button(root, text="9", command = lambda:button_pressed('9'))
+button9.grid(row=1, column=2,sticky='NSEW')
+
+button4 = ttk.Button(root, text="4", command = lambda:button_pressed('4'))
+button4.grid(row=2, column=0,sticky='NSEW')
+button5 = ttk.Button(root, text="5", command = lambda:button_pressed('5'))
+button5.grid(row=2, column=1,sticky='NSEW')
+button6 = ttk.Button(root, text="6", command = lambda:button_pressed('6'))
+button6.grid(row=2, column=2,sticky='NSEW')
+
+button1 = ttk.Button(root, text="1", command = lambda:button_pressed('1'))
+button1.grid(row=3, column=0,sticky='NSEW')
+button2 = ttk.Button(root, text="2", command = lambda:button_pressed('2'))
+button2.grid(row=3, column=1,sticky='NSEW')
+button3 = ttk.Button(root, text="3", command = lambda:button_pressed('3'))
+button3.grid(row=3, column=2,sticky='NSEW')
+#마지막줄 AC,0,=,- 버튼추가
+# -버튼 -> math_button_pressed() 로 연결.
+# AC,0 버튼 -> button_pressed() 로 연결
+# = 버튼 -> equal_button_pressed() 로 연결
+button_ac = ttk.Button(root, text="*", command = lambda:star_button_pressed('*'))
+button_ac.grid(row=4, column=0,sticky='NSEW')
+button0 = ttk.Button(root, text="0", command = lambda:button_pressed('0'))
+button0.grid(row=4, column=1,sticky='NSEW')
+button_equal = ttk.Button(root, text="#", command = lambda:num_button_pressed())
+button_equal.grid(row=4, column=2,sticky='NSEW') 
+ 
+root.mainloop()
---
+++ temp_server.py
@@ -0,0 +1,188 @@
+from socket import *
+from threading import Thread
+import sys
+import cv2
+import os
+from sys import platform
+import argparse
+import time
+import re
+import numpy
+
+def make_rect(height, width):
+
+        if(height > width):
+            #right hand
+            handRectangles = [
+                [
+                op.Rectangle(0., 0., 0., 0.),
+                op.Rectangle(0., (height - width)/2, width, width)
+                ]
+            ]
+
+        else:
+            #right hand
+            handRectangles = [
+                [
+                op.Rectangle(0., 0., 0., 0.),
+                op.Rectangle((width - height)/2, 0., height, height)
+                ]
+            ]
+
+        return handRectangles
+
+
+def run_openpose(opWrapper, addr, data):
+
+
+    # Construct it from system arguments
+    # op.init_argv(args[1])
+    # oppython = op.OpenposePython()
+
+    frame = data
+
+    height, width, channels = frame.shape
+
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+
+    video = cv2.VideoWriter(str(addr) + ".avi", fourcc, 30.0, (width, height))
+    out_file = open(str(addr) + ".txt", "a")
+
+    #cv2.imshow("VideoFrame", frame)
+
+    # Read image and face rectangle locations
+    #imageToProcess = cv2.imread(args[0].image_path)
+
+    # Create new datum
+    datum = op.Datum()
+    datum.cvInputData = frame
+    datum.handRectangles = make_rect(height, width)
+
+    # Process and display image
+    opWrapper.emplaceAndPop([datum])
+
+    out_file.write(str(datum.handKeypoints[1]))
+
+    #cv2.imshow("OpenPose 1.5.1 - Tutorial Python API", datum.cvOutputData) #show image
+
+    video.write(datum.cvOutputData)
+
+    out_file.close()
+    
+
+    return datum.cvOutputData, datum.handKeypoints[1]
+
+
+def recvall(sock, count):
+    buf = b''
+    while count:
+        newbuf = sock.recv(count)
+        
+        buf += newbuf
+        count -= len(newbuf)
+    return buf
+
+
+
+
+def data_receive(clientSock, addr):
+    command = ''
+    pwd = ""
+    while command != "*":
+            command = clientSock.recv(1).decode()
+            pwd += command
+            print(pwd)
+
+    print("out")
+    clientSock.send("1".encode())
+
+    '''
+    #openpose dir
+    dir_path="C:/Users/test/Desktop/openpose/build/examples/tutorial_api_python"
+
+    # Custom Params (refer to include/openpose/flags.hpp for more parameters)
+    params = dict()
+    params["model_folder"] = dir_path+"/../../../models/"
+    params["hand"] = True
+    params["hand_detector"] = 2
+    params["body"] = 0
+
+    # Starting OpenPose
+    opWrapper = op.WrapperPython()
+    opWrapper.configure(params)
+    opWrapper.start()
+
+    frame_size = clientSock.recv(30).decode()
+
+    frame_size = int(frame_size, 2)
+
+    print(frame_size)
+
+    frame = 0
+
+    while True:
+        
+        length = recvall(clientSock, 16)
+        stringData = recvall(clientSock, int(length))
+        data = numpy.fromstring(stringData, dtype = 'uint8')
+
+        imageToProcess = cv2.imdecode(data, cv2.IMREAD_COLOR)
+
+        print(str(frame) + "/" + str(frame_size))
+
+        img_result, skeleton_result = run_openpose(opWrapper, addr, imageToProcess)
+        
+        #cv2.imshow('result', img_result)
+        #cv2.waitKey(0)
+        
+        if(frame == frame_size):
+            print('read end')
+            break
+
+        frame += 1
+
+        #send img data
+        data = numpy.array(img_result)
+        stringData = data.tostring()
+        
+        #String 형태로 변환한 이미지를 socket을 통해서 전송
+        clientSock.sendall((str(len(stringData))).encode().ljust(16) + stringData)
+        print('send')
+        '''
+
+def run_server(host='127.0.0.1', port=8080):
+    with socket(AF_INET, SOCK_STREAM) as serverSock:
+        serverSock.bind(('', 8080))
+        while True:
+            serverSock.listen(5)
+            clientSock, addr = serverSock.accept()
+            # 새 연결이 생성되면 스레드를 만들어서 통신
+            t = Thread(target=data_receive, args=(clientSock, addr))
+            t.start()
+        serverSock.close()
+
+
+
+if __name__ == '__main__':
+
+    # Import Openpose (Windows/Ubuntu/OSX)
+    dir_path="C:/Users/test/Desktop/openpose/build/examples/tutorial_api_python"
+
+    try:
+        # Windows Import
+        if platform == "win32":
+            # Change these variables to point to the correct folder (Release/x64 etc.)
+            sys.path.append(dir_path + '/../../python/openpose/Release');
+            os.environ['PATH']  = os.environ['PATH'] + ';' + dir_path + '/../../x64/Release;' +  dir_path + '/../../bin;'
+            import pyopenpose as op
+        else:
+            # Change these variables to point to the correct folder (Release/x64 etc.)
+            sys.path.append('../../python');
+            # If you run `make install` (default path is `/usr/local/python` for Ubuntu), you can also access the OpenPose/python module from there. This will install OpenPose and the python library at your desired installation path. Ensure that this is in your python path in order to use it.
+            # sys.path.append('/usr/local/python')
+            from openpose import pyopenpose as op
+    except ImportError as e:
+        print('Error: OpenPose library could not be found. Did you enable `BUILD_PYTHON` in CMake and have this Python script in the right folder?')
+        raise e
+
+    run_server()
